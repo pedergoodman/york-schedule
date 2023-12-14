1db511aa995fb70042d94d594d44e957c9bfc478
JDNafz <nafziger10@gmail.com>
2023-12-14 12:48:33 -0600

    every minute

--- runner.js
+++ runner.js
@@ -8,7 +8,7 @@ const path = require("path");
 // ctrl + c
 
 // Set interval to 1 minutes (600,000 milliseconds)
-const minutes = 5; //test every how many minutes
+const minutes = 1; //test every how many minutes
 const seconds = 60; //60 means every minute (make this less for testing)
 const interval = minutes * seconds * 1000;
 // Execute the function initially and then at regular intervals
